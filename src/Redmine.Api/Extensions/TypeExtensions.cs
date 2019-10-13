c3b606609d9e24d5ea3255c90f8af001662f38c0
Zapadi <adi.popescu@gmail.com>
2019-10-13 03:17:31 +0300

    Add TypeExtensions

---
+++ src/Redmine.Api/Extensions/TypeExtensions.cs
@@ -0,0 +1,46 @@
+﻿/*
+   Copyright 2011 - 2016 Adrian Popescu
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+using System;
+
+namespace Redmine.Api.Extensions
+{
+    /// <summary>
+    /// /
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TInterface"></typeparam>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static bool Implements<TInterface>(this Type type) where TInterface : class
+        {
+            var interfaceType = typeof(TInterface);
+
+            if (!interfaceType.IsInterface)
+            {
+                throw new InvalidOperationException("Only interfaces can be implemented.");
+            }
+
+            return (interfaceType.IsAssignableFrom(type));
+        }
+    }
+
+}
