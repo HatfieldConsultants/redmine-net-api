9d6770f19c0305c5397e3d4abcf53cb52d6f201b
zapadi <adi.popescu@gmail.com>
2022-02-12 14:39:09 +0200

    Add RedmineSerializationType

---
+++ src/redmine-net-api/Serialization/RedmineSerializationType.cs
@@ -0,0 +1,16 @@
+namespace Redmine.Net.Api;
+
+/// <summary>
+/// 
+/// </summary>
+public enum RedmineSerializationType
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    Json,
+    /// <summary>
+    /// 
+    /// </summary>
+    Xml
+}
